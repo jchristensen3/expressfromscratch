9615fac78ac999bacead4c3d180d3e7d8f647d83
Julia Marie Christensen <jmfreder@up.com>
2021-05-14 11:32:40 -0500

    User model in place

---
+++ models/users.js
@@ -0,0 +1,13 @@
+const users = [ 
+    {
+        username: "julia christensen",
+        email: "jmfreder@up.com",
+        password: "hello333",
+        profile: "julia's page",
+        img: "https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        uuid: 12345
+    }
+
+]
+
+module.exports = users;
